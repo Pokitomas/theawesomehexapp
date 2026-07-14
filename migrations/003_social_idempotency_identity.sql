7e6f8dcf7d6ed94652b87a49a5258b2cda0c30fc
Pokitomas <130902344+Pokitomas@users.noreply.github.com>
2026-07-14 10:41:29 -0700

    Fail closed legacy social receipts

--- migrations/003_social_idempotency_identity.sql
+++ migrations/003_social_idempotency_identity.sql
@@ -3,10 +3,17 @@ BEGIN;
 ALTER TABLE social_mutation_receipts
   ADD COLUMN IF NOT EXISTS request_digest text;
 
+UPDATE social_mutation_receipts
+SET request_digest = repeat('0', 64)
+WHERE request_digest IS NULL;
+
+ALTER TABLE social_mutation_receipts
+  ALTER COLUMN request_digest SET NOT NULL;
+
 ALTER TABLE social_mutation_receipts
   DROP CONSTRAINT IF EXISTS social_mutation_receipts_request_digest_shape;
 ALTER TABLE social_mutation_receipts
   ADD CONSTRAINT social_mutation_receipts_request_digest_shape
-  CHECK (request_digest IS NULL OR request_digest ~ '^[a-f0-9]{64}$');
+  CHECK (request_digest ~ '^[a-f0-9]{64}$');
 
 COMMIT;
